220198b652c1b25ed67dd6c3a23fe58ecb76f401
Sany202 <dennissani86@gmail.com>
2023-08-29 07:03:50 -0700

    ses

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 :this frees a linked list
+ * free_listint2 :frees a linked list
  * @head: this points to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
