4bac2498474824db85500c2d7c83a7f6fcce63ec
Sanny202 <dennissani86@gmail.com>
2023-10-02 06:47:37 -0700

    t

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,27 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFFER_SIZE 1024
-
-/*standard libraries*/
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdlib.h>
-#include <stddef.h>
-#include <stdio.h>
-#include <errno.h>
-#include <elf.h>
 
 
-/*prototypes*/
-int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-size_t strlen(const char *s);
-/*void print_error(char *message);*/
-/*void print_usage(void);*/
 
 #endif
